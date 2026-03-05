a6776856c5c96c9f5884305078724b7175fa18cf
AnnaVitry <anne-marion119@hotmail.com>
2026-03-05 23:51:45 +0400

    dockercompose fonctionnel, avec persistance de donéé aprés un docker compose down et relancer docker compose up -d

--- app_api/modules/connect.py
+++ app_api/modules/connect.py
@@ -6,16 +6,16 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-# On récupère l'URL de la base depuis le .env
-# En local, on peut tester avec sqlite:///./test.db si Postgres n'est pas prêt
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+# On récupère l'URL Postgres du .env
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Création du moteur
+# Note : PostgreSQL n'a pas besoin de l'argument 'check_same_thread' de SQLite
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_db():
-    """Générateur de session de base de données."""
     db = SessionLocal()
     try:
         yield db
